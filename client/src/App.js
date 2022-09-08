99af56fce7272b37b71707e31f9f982f9685a05c
ticonetster <19826920+ticonetster@users.noreply.github.com>
2022-09-07 18:50:23 -0700

    mutations, queries and dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: '/graphql'
+});
 
 function App() {
   return (
